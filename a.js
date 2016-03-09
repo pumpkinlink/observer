c8bca846995cdcfcea788315b4c803e131418b9e
pumpkinlink <pumpkin_link@yahoo.com.br>
2016-03-08 23:43:28 -0300

    ...

--- a.js
+++ a.js
@@ -1,8 +1,32 @@
-function gerarListener(){
-    var obj, count = 0
-    return obj;
+function gerarSubject(){
+    var subject={count:  0}, observers=[]
+
+    subject.addObserver = function(obs){
+        observers.push(obs)
+    }
+
+    subject.update = function(){
+        this.count++
+        for(var i=0;i<observers.length;i++){
+            var listener = observers[i]
+            listener(subject)
+        }
+    }
+    return subject
 }
-var countOb = gerarListener();
-countOb.adicionarOuvinte(function(evento){
-        console.log(evento.contador)
+
+var countOb = gerarSubject()
+var listenerCount = (function(evento){
+    console.log('Count:' + evento.count)
 })
+
+var listenerCount2= (function(evento){
+    console.log('Count * 2 = ' + evento.count*2)
+})
+
+countOb.addObserver(listenerCount)
+countOb.addObserver(listenerCount2)
+
+countOb.update()
+countOb.update()
+countOb.update()
